5e2bb547e406aa835bd06cf5864211b4e625c34a
Tu Duan <uuxinhxinh2701@gmail.com>
2022-05-28 00:11:50 +0700

    Create service moduel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { StudentController } from 'src/student/student.controller';
+import { StudentService } from 'src/student/student.service';
 import { StudentTeacherController } from 'src/teacher/student.controller';
 import { TeacherController } from 'src/teacher/teacher.controller';
+import { TeacherService } from 'src/teacher/teacher.service';
 
 
 @Module({
   imports: [],
-  controllers: [StudentController, TeacherController, StudentTeacherController]
+  controllers: [StudentController, TeacherController, StudentTeacherController],
+  providers: [StudentService, TeacherService]
 })
 export class AppModule {}
