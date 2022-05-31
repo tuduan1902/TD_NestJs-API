fa0a925081c3d1cbcb9ec42598b0c8eabecd9482
Tu Duan <uuxinhxinh2701@gmail.com>
2022-05-31 22:49:51 +0700

    Write logic in student.service

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,26 +1,29 @@
 import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
 import { CreateStudentDto, UpdateStudentDto, FindStudentResponseDto, StudentResponseDto } from "./dto/student.dto"
+import { StudentService } from "./student.service";
 
 @Controller('students')
 export class StudentController {
+
+   constructor(private readonly studentService: StudentService) {}
    
    @Get()
    getStudents(): FindStudentResponseDto[] {
-      return "All Students"
+      return this.studentService.getStudents()
    }
 
    @Get('/:studentId')
    getStudentById(
       @Param('studentId') studentId: string
    ): FindStudentResponseDto {
-      return `Get Student With Id of ${studentId}`
+      return this.studentService.getStudentById(studentId)
    }
 
    @Post()
    createStudent(
       @Body() body: CreateStudentDto
    ): StudentResponseDto {
-      return `Create Student With The Following Data ${JSON.stringify(body)}`
+      return this.studentService.createStudent(body)
    }
 
    @Put('/:studentId')
@@ -28,6 +31,6 @@ export class StudentController {
       @Param('studentId') studentId: string,
       @Body() body: UpdateStudentDto 
    ): StudentResponseDto{
-      return `Update Student With Id of ${studentId} With Data of ${JSON.stringify(body)}`
+      return this.studentService.updateStudent(body, studentId)
    }
 }
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { students } from '../db';
+import { v4 as uuid } from 'uuid'
+import { CreateStudentDto, FindStudentResponseDto, StudentResponseDto, UpdateStudentDto } from './dto/student.dto';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+   private students = students;
+
+   getStudents(): FindStudentResponseDto[] {
+      return this.students
+   }
+
+   getStudentById(studentId: string): FindStudentResponseDto {
+      return this.students.find(student => {
+         return student.id === studentId
+      })
+   }
+
+   createStudent(payload: CreateStudentDto): StudentResponseDto {
+      let newStudent = {
+         id: uuid(),
+         ...payload
+      }
+      this.students.push(newStudent);
+
+      return newStudent
+   }
+
+   updateStudent(payload: UpdateStudentDto, studentId: string) {
+      let updateStudent: StudentResponseDto;
+
+      const updatedStudentList = this.students.map(student => {
+         if(student.id === studentId){
+            updateStudent = {
+               id: studentId,
+               ...payload
+            }
+         } else return student
+      });
+
+      this.students = updatedStudentList;
+      return updateStudent
+   }
+}
